2b0f07feb160ba2230c27b24269f0e013e6bb2ee
pranav-chenna <130360877+pranav-chenna@users.noreply.github.com>
2023-04-14 17:15:50 +0930

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+def main():
+    while True:
+        print("Welcome to My Application!")
+        print("1. Calculator")
+        choice = input("Enter your choice:")
+
+        if choice == "1":
+            calculator()
+
+        else:
+            print("Invalid choice. Please try again.")
+
+
+def calculator():
+    print("CALCULATOR")
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    operation = input("Enter the operation (1-4): ")
+    if operation == "1":
+        result = num1 + num2
+        print("The sum is:", result)
+    elif operation == "2":
+        result = num1 - num2
+        print("The difference is:", result)
+    elif operation == "3":
+        result = num1 * num2
+        print("The product is:", result)
+    elif operation == "4":
+        result = num1 / num2
+        print("The quotient is:", result)
+    else:
+        print("Invalid operation. Please try again.")
+
+
+
+if __name__ == '__main__':
+    import random
+
+    main()
